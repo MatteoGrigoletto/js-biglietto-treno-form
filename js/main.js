9e20e514dd6640c9b47f38328b0f3d16ba8c137c
MatteoGrigoletto <mttgrigoletto@gmail.com>
2022-10-21 15:44:03 +0200

    Aggiunge select e const js

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// button - Genera --> ticket calculation
+const generate = document.getElementById("generate");
+generate.addEventListener("click", function() {
+
+   // get values from inputs
+   const name = document.getElementById("name").value;
+   const distance = document.getElementById("distance").value;
+   const age = document.getElementById("age").value;
+console.log(distance)
+});
