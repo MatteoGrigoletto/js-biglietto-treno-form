44c8013147a6cb4a4d9bdb9658ed58eee7a85f55
MatteoGrigoletto <mttgrigoletto@gmail.com>
2022-10-21 16:22:36 +0200

    Aggiunge: - container -  d flex -ticket price

--- js/main.js
+++ js/main.js
@@ -6,5 +6,16 @@ generate.addEventListener("click", function() {
    const name = document.getElementById("name").value;
    const distance = document.getElementById("distance").value;
    const age = document.getElementById("age").value;
-console.log(distance)
+
+   // calculate ticket price
+  
+   let price = distance * 0.21;
+   if (age == "minorenne") {
+      price = (price * 0.8).toFixed(2);  
+   } else if (age == "over 65") {
+      price *= 0.6;
+   }
+   console.log(price)
+
+ 
 });
